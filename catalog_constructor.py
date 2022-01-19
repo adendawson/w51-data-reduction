76f876826dfddc5479528aa3fa1249bf49eaa712
Aden Dawson <adendawson@c0706a-s2.ufhpc>
2022-01-19 12:10:20 -0500

    added flux uncertainty

--- catalog_constructor.py
+++ catalog_constructor.py
@@ -10,16 +10,16 @@ from astropy.io import ascii
 def catalog_constructor(main_catalog, other_catalog, main_catalog_fh, other_catalog_fh, main_filter, other_filter):
     #main_catalog : catalog with highest population
     #other_catalog : catalog you want to add to main
-    #main_catalog_wcs : filehandle of main filter image (main_catalog_fh = fits.open(your_file_path_here))
-    #other_catalog_wcs : filehandle of other catalog image
+    #main_catalog_fh : filehandle of main filter image (main_catalog_fh = fits.open(your_file_path_here))
+    #other_catalog_fh : filehandle of other catalog image
     #main_filter (str) : one letter name of filter
     #other_filter (str) : one letter name of filter
     
     main_catalog_wcs = WCS(main_catalog_fh[0].header)
     other_catalog_wcs = WCS(other_catalog_fh[0].header)
     
-    ra_main, dec_main = main_catalog_wcs.all_pix2world(main_catalog['x_0'], main_catalog['y_0'], 0)*u.deg
-    ra_add, dec_add = other_catalog_wcs.all_pix2world(other_catalog['x_0'], other_catalog['y_0'], 0)*u.deg
+    ra_main, dec_main = main_catalog_wcs.all_pix2world(main_catalog['x_fit'], main_catalog['y_fit'], 0)*u.deg
+    ra_add, dec_add = other_catalog_wcs.all_pix2world(other_catalog['x_fit'], other_catalog['y_fit'], 0)*u.deg
     
     main_cat = SkyCoord(ra = ra_main, dec = dec_main)
     other_cat = SkyCoord(ra = ra_add, dec = dec_add)
@@ -32,20 +32,18 @@ def catalog_constructor(main_catalog, other_catalog, main_catalog_fh, other_cata
     for i in range(len(main_catalog)):
         main_catalog[f"{other_filter}band_match_number"][i] = index[i]
         main_catalog[f"{other_filter}band_match_separation"][i] = (d2d[i].to(u.arcsec)) / u.arcsec
-        
-    #main_catalog.rename_column('peak', f'peak_{main_filter}')
-    #main_catalog.rename_column('flux_0', f'flux_{main_filter}')
-    #main_catalog.rename_column('mag', f'mag_{main_filter}')
     
     indices = main_catalog[f'{other_filter}band_match_number']
     
     main_catalog.add_column(col=other_catalog['peak'][indices], name=f'peak_{other_filter}')
-    main_catalog.add_column(col=other_catalog['mag'][indices], name=f'mag_{other_filter}')
+    main_catalog.add_column(col=other_catalog[f'mag_{other_filter}'][indices], name=f'mag_{other_filter}')
     main_catalog.add_column(col=other_catalog['flux_0'][indices], name=f'flux_{other_filter}')
+    main_catalog.add_column(col=other_catalog['flux_unc'][indices], name=f'flux_unc_{other_filter}')
+    main_catalog.add_column(col=other_catalog[f'mag_{other_filter}_cal'][indices], name=f'mag_{other_filter}_cal')
     
     match_numbers = np.unique(main_catalog[f'{other_filter}band_match_number'])
 
-    main_catalog.add_column(col=np.ones(len(main_catalog), dtype='bool'), name=f'{other_filter}band_matched')
+    main_catalog.add_column(col=np.ones(len(main_catalog), dtype=bool), name=f'{other_filter}band_matched')
     
     for idnumber in match_numbers:
         selection = main_catalog[f'{other_filter}band_match_number'] == idnumber
